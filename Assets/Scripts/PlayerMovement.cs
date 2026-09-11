067f77b8b7d2145c4c334916c8e9c36682644ce9
lua-p1 <luapitra@gmail.com>
2026-09-11 11:22:02 -0300

    /same

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] Contoller _controller;
+    [SerializeField] float _speed;
+
+    private void Update()
+    {
+        
+    }
 }
