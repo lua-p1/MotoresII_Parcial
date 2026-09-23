7aeeefc27fff4a3e7482d8d495ecbab7d3958f7d
Nico <nicolas.migues97@gmail.com>
2026-09-22 23:12:54 -0300

    OKWINNING

--- Assets/Scripts/UI/EndScreenUI.cs
+++ Assets/Scripts/UI/EndScreenUI.cs
@@ -4,27 +4,22 @@ public class EndScreenUI : MonoBehaviour
 {
     [SerializeField] private EventType showOnEvent;
     [SerializeField] private GameObject panel;
-
     private void Awake()
     {
         panel.SetActive(false);
     }
-
     private void OnEnable()
     {
         EventManager.SubscribeToEvent(showOnEvent, Show);
     }
-
     private void OnDisable()
     {
         EventManager.UnsubscribeToEvent(showOnEvent, Show);
     }
-
     private void Show(params object[] parameters)
     {
         panel.SetActive(true);
     }
-
     public void Restart()
     {
         Debug.Log("Reinciar");
